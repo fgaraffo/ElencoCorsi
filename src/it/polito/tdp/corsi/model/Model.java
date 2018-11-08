f2fd474d0670b7ea5abd69eee08441eecf02c08b
fedeg <fedeg@Hp-Fede.lan>
2018-11-08 16:57:28 +0100

    soluzione metodo listAll (con filtro risultato nel model)

---
+++ src/it/polito/tdp/corsi/model/Model.java
@@ -0,0 +1,35 @@
+package it.polito.tdp.corsi.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.polito.tdp.corsi.db.CorsoDAO;
+
+public class Model {
+
+	private List <Corso> corsi;
+	
+	public List <Corso> listaCorsiSemestre (int pd)
+	{
+		CorsoDAO dao = new CorsoDAO();
+		
+		// opzione 1: leggo tutto e filtro qui
+		this.corsi = dao.listAll();
+		List <Corso> risultato = new ArrayList<>();
+		for (Corso c : this.corsi)
+		{
+			if (c.getPd() == pd)
+			{
+				risultato.add(c);
+			}
+		}
+		return risultato;
+
+		// opzione 2: lo fa il database
+	
+	//	List <Corso> risultato2 = dao.listByPD(pd);
+	//	return risultato2;
+	}
+	
+	
+}
